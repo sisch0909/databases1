9d289c74a864e18954e859d2adb8be4aa91de1c0
sisch0909 <sisch0909@users.noreply.github.com>
2021-05-18 19:42:25 +0200

    Finished data.sql except for members

--- sql/data.sql
+++ sql/data.sql
@@ -1,7 +1,7 @@
 insert into age_groups (age_group_ID, age_group_name, discount) values (1, 'kids', 50);
 insert into age_groups (age_group_ID, age_group_name, discount) values (2, 'teenagers', 30);
 insert into age_groups (age_group_ID, age_group_name, discount) values (3, 'adults', 0);
-insert into age_groups (age_group_ID, age_group_name, discount) values (1, 'seniors', 20);
+insert into age_groups (age_group_ID, age_group_name, discount) values (4, 'seniors', 20);
 
 insert into sports (sport_ID, sport_name, sport_type, professionalism) values (1, 'soccer', 'team', TRUE);
 insert into sports (sport_ID, sport_name, sport_type, professionalism) values (2, 'soccer', 'team', FALSE);
@@ -20,7 +20,7 @@ insert into professionalisms (professionalism_ID, prof_name, discount) values (1
 insert into professionalisms (professionalism_ID, prof_name, discount) values (2, 'hobby', 0);
 
 insert into roles (role_ID, role_name, all_rights, most_rights, some_rights) values (1, 'admin', TRUE, FALSE, FALSE);
-insert into roles (role_ID, role_name, all_rights, most_rights, some_rights) values (2, 'admin', FALSE, TRUE, FALSE);
+insert into roles (role_ID, role_name, all_rights, most_rights, some_rights) values (2, 'president', FALSE, TRUE, FALSE);
 insert into roles (role_ID, role_name, all_rights, most_rights, some_rights) values (3, 'chairman', FALSE, FALSE, TRUE);
 
 insert into duration_groups (duration_group_ID, duration_group_name, discount) values (1, 'newbies', 0);
@@ -28,8 +28,8 @@ insert into duration_groups (duration_group_ID, duration_group_name, discount) v
 insert into duration_groups (duration_group_ID, duration_group_name, discount) values (3, 'longtimers', 20);
 insert into duration_groups (duration_group_ID, duration_group_name, discount) values (4, 'legends', 30);
 
-insert into memberships (membership_ID, membership_name, discount) values (1, 'active member', 0):
-insert into memberships (membership_ID, membership_name, discount) values (2, 'passive member', 50):
+insert into memberships (membership_ID, membership_name, discount) values (1, 'active', 0);
+insert into memberships (membership_ID, membership_name, discount) values (2, 'passive', 50);
 
 
 
