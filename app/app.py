70ffce9f7a12c292eaa45aa42e3a0d311d651919
adichter-hpe <andreas.dichter@hpe.com>
2021-05-20 14:33:15 +0200

    sicherheits push (will was testen)

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_restful import Resource, Api
+import pandas as pd
 
 app = Flask(__name__)
 api = Api(app)
+
+
 
 @app.route('/')
 def index():
@@ -15,6 +18,10 @@ def view():
 @app.route('/edit')
 def edit():
     return render_template("edit.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
