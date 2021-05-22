689f1acf155551501315a7c14d7a6f8abb1cec56
adichter-hpe <andreas.dichter@hpe.com>
2021-05-22 21:51:23 +0200

    code clean up + major webapp upgrade

--- app/app.py
+++ app/app.py
@@ -32,71 +32,33 @@ Memberships = Base.classes.memberships
 #print(Base.classes.keys())
 #print(db.query(Members).filter_by(email='giles.leonard560@mail.com').first())
 
+#For string in navbar
+logged_in = False
+if logged_in == False:
+    nav_string = "My personal data"
+else:
+    user_id = session["user_id"]
+    nav_name = db.session.query(Members_info).filter_by(member_id=user_id).first()
+    nav_string = nav_name.first_name + " " + nav_name.last_name
 
 members = pd.read_csv("members.csv", sep=",")
 
-@app.route('/')
-def login():
-    
-    return render_template("login.html", members=members)
 
-@app.route('/view')
-def view():
-    user_id = session["user_id"]
-    user_data = db.session.query(Members_info).filter_by(member_id=user_id).first()
-
-    return render_template("view.html", members=members, user_data = user_data)
-
-
-@app.route('/')
-def logout():
-    session.clear()
-    return render_template("login.html")
-
-@app.route('/edit', methods=[ 'POST', 'GET'])
-def edit():
-    user_id = session["user_id"]
-    user_info_data = db.session.query(Members_info).filter_by(member_id=user_id).first()
-    
-    #get info from data
-    firstname = request.form.get("firstname")
-    lastname = request.form.get("lastname")
-    gender = request.form.get("gender")
-    phone = request.form.get("phone")
-    #email = request.form.get("email")
-
-    
-    
-
-    
-
-    if request.method == "POST":
-        user_firstname = request.form['firstname']
-        user_lastname = request.form['lastname']
-
-        #push to database
-        #user_info_data.first_name = firstname
-        user_info_data.last_name = lastname
-        #user_info_data.gender = gender
-        #user_info_data.phone_number = phone
-    #   user_info_data.email = email
-        db.session.commit()
-        update_statement=" Your data has been succesfully updated!"
-        return render_template("edit.html", update_statement=update_statement, members=members, user_info_data = user_info_data)
-
-
-    return render_template("edit.html", members=members, user_info_data = user_info_data)
-
+#Home
 @app.route('/home')
 def home():
-    return render_template("home.html", members=members)
+    return render_template("home.html", nav_string=nav_string)
+
+
+#Loginpage
+@app.route('/')
+def login():
+    return render_template("login.html", nav_string=nav_string)
 
 @app.route('/login', methods=["POST"])
 def loginpage():
     email = request.form.get("email")
     password = request.form.get("password")
-    #id=0
-    #i=0
     error_statement=""
     exists = db.session.query(db.exists().where(Members.email == email)).scalar()
 
@@ -105,66 +67,192 @@ def loginpage():
         return render_template("login.html", 
         email=email, 
         password=password, 
-        error_statement=error_statement)
+        error_statement=error_statement,
+        nav_string=nav_string)
     elif exists == True:
         user = db.session.query(Members).filter_by(email=email).first()
         user_info = db.session.query(Members_info).filter_by(email=email).first()
         if user.pw == password:
             session["user_id"] = user_info.member_id
             nachricht = session["user_id"]
-            return render_template("welcome.html", nachricht=nachricht)
+            logged_in = True
+            return render_template("welcome.html", nachricht=nachricht, nav_string=nav_string)
         else:
             error_statement = "Wrong email or password"
             return render_template("login.html", 
             email=email, 
             password=password, 
-            error_statement=error_statement)
+            error_statement=error_statement,
+            nav_string=nav_string)
     else:
         error_statement = "Wrong email or password"
         return render_template("login.html", 
         email=email, 
         password=password,
-        error_statement=error_statement)
-    
-        
-    #while i == 1:
-      #  if email == members.email.id:
-       #     if password == members.pw.id:
-        #        #return render_template("welcome.html", members=members)
-         #       i=0
-        #else:
-        #    id=+1
-        #    i=1
-        #    return render_template("wrongpwd.html", member=len(members))
+        error_statement=error_statement,
+        nav_string=nav_string)
 
+
+#Logout
+@app.route('/')
+def logout():
+    session.clear()
+    nav_string = "My personal data"
+    logged_out()
+    return render_template("login.html", nav_string=nav_string)
+
+
+#View data
+@app.route('/view')
+def view():
+    user_id = session["user_id"]
+    user_data = db.session.query(Members_info).filter_by(member_id=user_id).first()
+    return render_template("view.html", user_data = user_data, nav_string=nav_string)
+
+
+#edit data
 @app.route('/edit', methods=["POST"])
 def update_data():
     email = request.form.get("email")
     password = request.form.get("password")
     update_statement=" Your data has been succesfully updated!"
-    return render_template("edit.html", update_statement=update_statement, members=members)
+    return render_template("edit.html", 
+    update_statement=update_statement, 
+    members=members, 
+    nav_string=nav_string)
 
+@app.route('/edit', methods=['POST', 'GET'])
+def edit():
+    user_id = session["user_id"]
+    user_info_data = db.session.query(Members_info).filter_by(member_id=user_id).first()
+    
+    #get info from html
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    gender = request.form.get("gender")
+    phone = request.form.get("phone")
+    #email = request.form.get("email")
+
+    if request.method == "POST":
+        if request.form.get("update"):
+            #push to database
+            #user_info_data.first_name = firstname
+            
+            #user_info_data.gender = gender
+            #user_info_data.phone_number = phone
+    #       user_info_data.email = email
+            user_info_data.last_name = lastname
+            db.session.commit()
+            update_statement = "Your data has been succesfully updated!"
+            return render_template("edit.html", 
+            update_statement=update_statement, 
+            members=members, 
+            user_info_data=user_info_data,
+            nav_string=nav_string)
+        elif request.form.get("delete"):
+            update_statement="Your account has been succesfully deleted!"
+            return render_template("edit.html", 
+            update_statement=update_statement, 
+            members=members, 
+            user_info_data=user_info_data,
+            nav_string=nav_string)
+        
+
+
+    return render_template("edit.html", 
+    members=members, 
+    user_info_data=user_info_data,
+    nav_string=nav_string)
+
+
+#Create new account
 @app.route('/createaccount')
 def create_account():
+    return render_template("create_account.html", nav_string=nav_string)
+
+@app.route('/createaccount', methods=["POST"])
+def really_create_account():
     firstname = request.form.get("firstname")
     lastname = request.form.get("lastname")
     gender = request.form.get("gender")
     phone = request.form.get("phone")
     membership_type = request.form.get("membership_type")
-    #birthdate = request.form.get("birthdate")
-    #email = request.form.get("email")
+    birthdate = request.form.get("birthdate")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
     if request.method == "POST":
-        new_member1 = Members_info(email="fgehfkshfsd@fsd.com", first_name=firstname, last_name=lastname, phone_number=phone, birthday="2000-01-01", gender=gender, membership_type_id=membership_type, duration_group=1, role_id=4)
-        db.session.add(new_member1)
-        db.session.commit()
+        #new_member1 = Members_info(email="fgehfkshfsd@fsd.com", first_name=firstname, last_name=lastname, phone_number=phone, birthday="2000-01-01", gender=gender, membership_type_id=membership_type, duration_group=1, role_id=4)
+        #db.session.add(new_member1)
+        #db.session.commit()
         update_statement="Application has been sent. You can now login."
-        return render_template("create_account.html", update_statement=update_statement)
+        return render_template("create_account.html",
+            firstname = firstname,
+            lastname = lastname,
+            gender = gender,
+            phone = phone,
+            membership_type = membership_type,
+            #sport = sport,
+            birthdate = birthdate,
+            email = email,
+            password = password, 
+            update_statement=update_statement,
+            nav_string=nav_string)
 
-    return render_template("create_account.html")
 
+#reset password
 @app.route('/resetpassword')
 def reset_password():
-    return render_template("reset_password.html", members=members)
+    return render_template("reset_password.html", nav_string=nav_string)
+
+@app.route('/resetpassword', methods=["POST"])
+def really_reset_password():
+    email = request.form.get("email")
+    password = request.form.get("old_password")
+    new_password = request.form.get("new_password")
+
+    error_statement=""
+    exists = db.session.query(db.exists().where(Members.email == email)).scalar()
+
+    if not email or not password or not new_password:
+        error_statement = "Please fill in missing fields"
+        return render_template("reset_password.html", 
+        email=email, 
+        password=password, 
+        error_statement=error_statement,
+        nav_string=nav_string)
+    elif exists == True:
+        user = db.session.query(Members).filter_by(email=email).first()
+        user_info = db.session.query(Members_info).filter_by(email=email).first()
+        if user.pw == password:
+            #DB connection
+            session["user_id"] = user_info.member_id
+            user_id = session["user_id"]
+            user_info_data = db.session.query(Members_info).filter_by(member_id=user_id).first()
+            user_data = db.session.query(Members).filter_by(email = user_info_data.email).first()
+            
+            nachricht = session["user_id"]
+            logged_in = True
+            
+            if request.method == "POST":
+                user_data.pw = new_password
+                db.session.commit()
+                return render_template("welcome.html", nachricht=nachricht, nav_string=nav_string)
+        else:
+            error_statement = "Wrong email or password"
+            return render_template("reset_password.html", 
+            email=email, 
+            password=password, 
+            error_statement=error_statement,
+            nav_string=nav_string)
+    else:
+        error_statement = "Wrong email or password"
+        return render_template("reset_password.html", 
+        email=email, 
+        password=password,
+        error_statement=error_statement,
+        nav_string=nav_string)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
